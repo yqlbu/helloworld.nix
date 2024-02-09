c05985649d25337d06627325dc448221285c4933
yqlbu <yqlbu@bu.edu>
2024-02-09 15:30:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# helloworld.nix
+
+## Build
+
+```bash
+nix build .
+```
+
+## Run
+
+```bash
+# run with default package
+nix run
+# run with specified package
+nix run .#vim
+```
---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic helloworld flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+        packageName = "helloworld";
+      in {
+        # build package
+        packages.${packageName} = with pkgs;
+          stdenv.mkDerivation rec {
+            name = packageName;
+            version = "v1.0.0";
+            src = self;
+            buildPhase = "gcc -o ${name} ./hello.c";
+            installPhase = "mkdir -p $out/bin; install -t $out/bin ${name}";
+          };
+
+        # define output packages
+        packages = {
+          default = self.packages.${system}.${packageName};
+          vim = pacakges.legacyPackages.${system}.vim;
+        };
+
+        # dev environment
+        devShells.default = with pkgs; mkShell {
+          packages = [ cowsay vim ];
+        };
+      });
+}
+
